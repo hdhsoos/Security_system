a083bb8313ac2be9f93ffa4aed9e35c210b5bc36
Мария <andreeva-mm03@yandex.ru>
2024-05-18 23:42:36 +0300

    Оформила второе окно

--- Database.py
+++ Database.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 # создаём класс для работы с базой данных
-class DB:
+class LoginsDB:
     # конструктор класса
     def __init__(self):
         # соединяемся с файлом базы данных
@@ -59,6 +59,64 @@ class DB:
         rows = self.cur.fetchall()
         return rows
 
+class WorkersDB:
+    # конструктор класса
+    def __init__(self):
+        # соединяемся с файлом базы данных
+        self.conn = sqlite3.connect("Security.db")
+        # создаём курсор для виртуального управления базой данных
+        self.cur = self.conn.cursor()
+        # если нужной нам таблицы в базе нет — создаём её
+        self.cur.execute(
+            "CREATE TABLE IF NOT EXISTS workers (id INTEGER PRIMARY KEY, fio TEXT, cabinets TEXT)") # '1 3 4 5' - массив цифр
+        # сохраняем сделанные изменения в базе
+        self.conn.commit()
+
+        # деструктор класса
+
+    def __del__(self):
+        # отключаемся от базы при завершении работы
+        self.conn.close()
+
+        # просмотр всех записей
+
+    def view(self):
+        # выбираем все записи о покупках
+        self.cur.execute("SELECT * FROM workers")
+        # собираем все найденные записи в колонку со строками
+        rows = self.cur.fetchall()
+        # возвращаем сроки с записями расходов
+        return rows
+
+    # добавляем новую запись
+    def insert(self, fio, cabinets):
+        # формируем запрос с добавлением новой записи в БД
+        self.cur.execute("INSERT INTO workers VALUES (NULL,?,?,?)", (fio, cabinets,))
+        # сохраняем изменения
+        self.conn.commit()
+
+    # обновляем информацию о покупке
+    def update(self, id, fio, cabinets):
+        # формируем запрос на обновление записи в БД
+        self.cur.execute("UPDATE workers SET fio=?, cabinets=? WHERE id=?", (fio, cabinets, id,))
+        # сохраняем изменения
+        self.conn.commit()
+
+    # удаляем запись
+    def delete(self, id):
+        # формируем запрос на удаление выделенной записи по внутреннему порядковому номеру
+        self.cur.execute("DELETE FROM workers WHERE id=?", (id,))
+        # сохраняем изменения
+        self.conn.commit()
+
+    # ищем запись по названию покупки
+    def search(self, login=""):
+        # формируем запрос на поиск по точному совпадению
+        self.cur.execute("SELECT * FROM workers WHERE fio=?", (login,))
+        # формируем полученные строки и возвращаем их как ответ
+        rows = self.cur.fetchall()
+        return rows
 
 # создаём новый экземпляр базы данных на основе класса
-passwords = DB()
+passwords = LoginsDB()
+workers = WorkersDB()
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from Database import *
 
 
 def wrong_update(login, password):
-    text_elem = window['-result-'] # получаем доступ к текстовому элементу
+    text_elem = window['-result-']  # получаем доступ к текстовому элементу
     search = passwords.search(login)
     if search == []:
         text_elem.update("Логин не найден.")
@@ -12,18 +12,27 @@ def wrong_update(login, password):
     elif search[0][2] == password:
         open_main_wind()
 
+
 def open_main_wind():
     global layout, window
 
     window.close()
-    layout = [[sg.Text('Добро пожаловать', pad=((0, 0), (200, 8)))]]
-    window = sg.Window('Security program', layout, location=(300, 0), finalize=True, element_justification="center",
-                       size=(600, 550))
+    layout = [[sg.Text('Карта', pad=((133, 0), (8, 8)))],
+              [sg.Button('Кабинет 1', size=(10, 5), pad=((5, 10), (0, 0))),
+               sg.Button('Кабинет 2', size=(10, 5), pad=((5, 10), (0, 0))), sg.Button('Кабинет 3', size=(10, 5)), sg.Button('Сотрудники', size=(15, 2), pad=((27, 0), (0, 0)))],
+              [sg.Button('Кабинет 4', size=(10, 5), pad=((5, 10), (0, 0))),
+              sg.Button('Кабинет 5', size=(10, 5), pad=((5, 10), (0, 0))), sg.Button('Кабинет 6', size=(10, 5)), sg.Button('Открыть журнал', size=(15, 2), pad=((27, 0), (0, 0)))],
+              [sg.Button('Кабинет 7', size=(10, 5), pad=((5, 10), (0, 0))), sg.Button('Кабинет 8', size=(10, 5),
+                                                                                      pad=((5, 10), (0, 0))), sg.Button(
+                  'Кабинет 9', size=(10, 5)), sg.Button('Добавить нового охранника', size=(15, 2), pad=((27, 0), (0, 0)))], [sg.Button('Cancel', pad=((205, 0), (30, 0)))]]
+    window = sg.Window('Security program', layout, location=(300, 0), finalize=True,
+                       size=(480, 400))#, sg.Button('Добавить нового сотрудника', size=(15, 2), pad=((160, 0), (6, 0)))]
+
 
 # All the stuff inside your window.
 layout = [[sg.Text('Введи логин и пароль', pad=((0, 0), (200, 8)))],
-          [sg.Text('Логин:   '), sg.InputText()],
-          [sg.Text('Пароль: '), sg.InputText()],
+          [sg.Text('Логин:   '), sg.InputText(size=(16, 1))],
+          [sg.Text('Пароль: '), sg.InputText(size=(16, 1))],
           [sg.Button('Ok'), sg.Button('Cancel')],
           [sg.Text('', key='-result-')]]
 
@@ -35,6 +44,7 @@ while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Cancel':  # if user closes window or clicks cancel
         break
-    wrong_update(values[0], values[1])
+    if event == 'Ok':
+        wrong_update(values[0], values[1])
 
 window.close()
