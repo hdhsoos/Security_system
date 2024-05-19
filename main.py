5a892d8b0030534267f27de83715d2b9d9f181f1
Мария <andreeva-mm03@yandex.ru>
2024-05-19 21:58:25 +0300

    подготовила программу к превращению в exe

--- main.py
+++ main.py
@@ -1,6 +1,152 @@
 import PySimpleGUI as sg
-from Database import *
+#from Database import *
 import time
+import sqlite3
+
+# создаём класс для работы с базой данных
+class LoginsDB:
+    # конструктор класса
+    def __init__(self):
+        # соединяемся с файлом базы данных
+        self.conn = sqlite3.connect("Security.db")
+        # создаём курсор для виртуального управления базой данных
+        self.cur = self.conn.cursor()
+        # если нужной нам таблицы в базе нет — создаём её
+        self.cur.execute(
+            "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, login TEXT, password TEXT)")
+        # сохраняем сделанные изменения в базе
+        self.conn.commit()
+
+        # деструктор класса
+
+    def __del__(self):
+        # отключаемся от базы при завершении работы
+        self.conn.close()
+
+        # просмотр всех записей
+
+    def view(self):
+        # выбираем все записи о покупках
+        self.cur.execute("SELECT * FROM users")
+        # собираем все найденные записи в колонку со строками
+        rows = self.cur.fetchall()
+        # возвращаем сроки с записями расходов
+        return rows
+
+    # добавляем новую запись
+    def insert(self, login, password):
+        # формируем запрос с добавлением новой записи в БД
+        self.cur.execute("INSERT INTO users VALUES (NULL,?,?)", (login, password,))
+        # сохраняем изменения
+        self.conn.commit()
+
+    # обновляем информацию о покупке
+    def update(self, id, login, password):
+        # формируем запрос на обновление записи в БД
+        self.cur.execute("UPDATE users SET login=?, password=? WHERE id=?", (login, password, id,))
+        # сохраняем изменения
+        self.conn.commit()
+
+    # удаляем запись
+    def delete(self, id):
+        # формируем запрос на удаление выделенной записи по внутреннему порядковому номеру
+        self.cur.execute("DELETE FROM users WHERE id=?", (id,))
+        # сохраняем изменения
+        self.conn.commit()
+
+    # ищем запись по названию покупки
+    def search(self, login=""):
+        # формируем запрос на поиск по точному совпадению
+        self.cur.execute("SELECT * FROM users WHERE login=?", (login,))
+        # формируем полученные строки и возвращаем их как ответ
+        rows = self.cur.fetchall()
+        return rows
+
+class WorkersDB:
+    # конструктор класса
+    def __init__(self):
+        # соединяемся с файлом базы данных
+        self.conn = sqlite3.connect("Security.db")
+        # создаём курсор для виртуального управления базой данных
+        self.cur = self.conn.cursor()
+        # если нужной нам таблицы в базе нет — создаём её
+        self.cur.execute(
+            "CREATE TABLE IF NOT EXISTS workers (id INTEGER PRIMARY KEY, fio TEXT, cabinets TEXT, cur_place TEXT)") # '1 3 4 5' - массив цифр
+        # сохраняем сделанные изменения в базе
+        self.conn.commit()
+
+        # деструктор класса
+
+    def __del__(self):
+        # отключаемся от базы при завершении работы
+        self.conn.close()
+
+        # просмотр всех записей
+
+    def view(self):
+        # выбираем все записи о покупках
+        self.cur.execute("SELECT * FROM workers")
+        # собираем все найденные записи в колонку со строками
+        rows = self.cur.fetchall()
+        # возвращаем сроки с записями расходов
+        return rows
+
+    # добавляем новую запись
+    def insert(self, fio, cabinets, cur_place):
+        # формируем запрос с добавлением новой записи в БД
+        self.cur.execute("INSERT INTO workers VALUES (NULL,?,?,?)", (fio, cabinets, cur_place,))
+        # сохраняем изменения
+        self.conn.commit()
+
+    # обновляем информацию о покупке
+    def update(self, id, fio, cabinets, cur_place):
+        # формируем запрос на обновление записи в БД
+        self.cur.execute("UPDATE workers SET fio=?, cabinets=?, cur_place=? WHERE id=?", (fio, cabinets, cur_place, id,))
+        # сохраняем изменения
+        self.conn.commit()
+
+    # удаляем запись
+    def delete(self, id):
+        # формируем запрос на удаление выделенной записи по внутреннему порядковому номеру
+        self.cur.execute("DELETE FROM workers WHERE id=?", (id,))
+        # сохраняем изменения
+        self.conn.commit()
+
+    # ищем запись по названию покупки
+    def search(self, login=""):
+        # формируем запрос на поиск по точному совпадению
+        self.cur.execute("SELECT * FROM workers WHERE fio=?", (login,))
+        # формируем полученные строки и возвращаем их как ответ
+        rows = self.cur.fetchall()
+        return rows
+
+    def search_id(self, id=""):
+        # формируем запрос на поиск по точному совпадению
+        self.cur.execute("SELECT * FROM workers WHERE id=?", (id,))
+        # формируем полученные строки и возвращаем их как ответ
+        rows = self.cur.fetchall()
+        return rows
+
+    def search_second(self, cur_place=""):
+        # формируем запрос на поиск по точному совпадению
+        self.cur.execute("SELECT * FROM workers WHERE cur_place=?", (cur_place,))
+        # формируем полученные строки и возвращаем их как ответ
+        rows = self.cur.fetchall()
+        return rows
+
+# создаём новый экземпляр базы данных на основе класса
+passwords = LoginsDB()
+if passwords.search('a') == []:
+    passwords.insert('a', 'a')
+workers = WorkersDB()
+if workers.search('Смирнова Майя Ильинична') == []:
+    workers.insert('Смирнова Майя Ильинична', '1 3 5', '2')
+if workers.search('Лебедева Полина Петровна') == []:
+    workers.insert('Лебедева Полина Петровна', '1 2 6', '1')
+if workers.search('Царев Макар Ильич') == []:
+    workers.insert('Царев Макар Ильич', '6 7 8 9', '6')
+if workers.search('Семенов Кирилл Алексеевич') == []:
+    workers.insert('Семенов Кирилл Алексеевич', '5 6 7 8', '5')
 
 def wrong_update(login, password):
     text_elem = window['-result-']  # получаем доступ к текстовому элементу
@@ -87,11 +233,19 @@ def open_workers_list():
 
 def open_log():
     global window
-
+    A = []
+    try:
+        f = open('log.txt', 'r')
+    except:
+        f = open('log.txt', 'w')
+        f.close()
+        f = open('log.txt', 'r')
+    for el in f:
+        A.append(el)
     window.close()
-    layout = [[sg.Text('Здесь будет журнал')], [sg.Button('Back', pad=((205, 0), (30, 0)))]]
+    layout = [[sg.Listbox(A, size=(75, 20), pad=((2, 0), (10, 10)))], [sg.Button('Back', pad=((250, 0), (15, 0)))]]
     window = sg.Window('Журнал', layout, location=(300, 0), finalize=True,
-                       size=(480, 400))
+                       size=(580, 420))
 
 
 def find_worker(fio):
